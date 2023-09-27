2c5e0318ac548b8df0dfcd20ec3f910996b1cbc9
Sua Qi Rong <qirong36@gmail.com>
2023-09-27 17:42:30 +0800

    Add search bar function

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,44 @@
 "use client";
+import { useState } from "react";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog";
+    ChevronLeft,
+    ChevronRight,
+    X,
+    Search,
+    ListFilter,
+    Bell,
+    MessageSquare,
+} from "lucide-react";
 
 export default function home() {
     return (
-        <div>
-            <Dialog>
-                <DialogTrigger>Open</DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Are you sure absolutely sure?</DialogTitle>
-                        <DialogDescription>
-                            This action cannot be undone. This will permanently
-                            delete your account and remove your data from our
-                            servers.
-                        </DialogDescription>
-                    </DialogHeader>
-                </DialogContent>
-            </Dialog>
+        <div className="w-full p-10">
+            <div className="flex flex-row items-center justify-between pl-5 pr-5">
+                <div className="flex flex-row">
+                    <ChevronLeft />
+                    <ChevronRight />
+                </div>
+
+                <div className="flex flex-row items-center">
+                    <div className="flex border-2 items-center rounded-xl overflow-hidden">
+                        <Search className="ml-2" />
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className="px-4 py-2 w-full focus:outline-none"
+                        />
+                        <X className="mr-2" />
+                    </div>
+                    <ListFilter className="ml-3" />
+                </div>
+
+                <div className="flex flex-row gap-4">
+                    <Bell />
+                    <MessageSquare />
+                </div>
+            </div>
+
+            <div></div>
         </div>
     );
 }
