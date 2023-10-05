ebe645286ef97da0bfb8704e218a7b8855da1d5d
Matthias <matthiasngeo@gmail.com>
2023-10-05 22:59:32 +0800

    Add missing like table & cascading deletes

--- migrations/V1__Initial.sql
+++ migrations/V1__Initial.sql
@@ -9,8 +9,8 @@ CREATE TABLE user_profile (
 -- This isn't the most optimal way to represent a bidirectional relationship but KISS.
 CREATE TABLE user_profile_user_profile (
     PRIMARY KEY (first_user_id, second_user_id),
-    first_user_id text REFERENCES user_profile(id) DEFERRABLE INITIALLY DEFERRED NOT NULL,
-    second_user_id text REFERENCES user_profile(id) DEFERRABLE INITIALLY DEFERRED NOT NULL,
+    first_user_id text REFERENCES user_profile(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL,
+    second_user_id text REFERENCES user_profile(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL,
 
     pending bool DEFAULT true NOT NULL -- Once again, this should be an enum but KISS.
 );
@@ -19,7 +19,7 @@ CREATE TABLE user_profile_user_profile (
 
 CREATE TABLE book_rating (
     PRIMARY KEY (user_id, isbn),
-    user_id text REFERENCES user_profile(id) DEFERRABLE INITIALLY DEFERRED NOT NULL,
+    user_id text REFERENCES user_profile(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL,
     isbn text NOT NULL,
 
     rating double precision,
@@ -30,7 +30,7 @@ CREATE TABLE book_rating (
 
 CREATE TABLE book_comment (
     PRIMARY KEY (user_id, comment_id),
-    user_id text REFERENCES user_profile(id) DEFERRABLE INITIALLY DEFERRED NOT NULL,
+    user_id text REFERENCES user_profile(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL,
     comment_id uuid NOT NULL,
 
     isbn text NOT NULL,
@@ -53,26 +53,32 @@ CREATE TABLE user_group (
 
 CREATE TABLE user_group_user_profile (
     PRIMARY KEY (group_id, user_id),
-    group_id text REFERENCES user_group(id) DEFERRABLE INITIALLY DEFERRED NOT NULL,
-    user_id text REFERENCES user_profile(id) DEFERRABLE INITIALLY DEFERRED NOT NULL,
+    group_id text REFERENCES user_group(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL,
+    user_id text REFERENCES user_profile(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL,
 
     permissions text[] DEFAULT '{}' NOT NULL -- This should be a separate table if it's actual production code but KISS.
 );
 
 CREATE TABLE user_group_discussion (
     PRIMARY KEY (group_id, discussion_id),
-    group_id text REFERENCES user_group(id) DEFERRABLE INITIALLY DEFERRED NOT NULL,
+    group_id text REFERENCES user_group(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL,
     discussion_id UUID NOT NULL,
 
     name text NOT NULL,
-    creator_id text REFERENCES user_profile(id) DEFERRABLE INITIALLY DEFERRED NOT NULL
+    creator_id text REFERENCES user_profile(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL
 );
 
 CREATE TABLE user_group_discussion_comment (
     PRIMARY KEY (discussion_id, comment_id),
-    discussion_id UUID REFERENCES user_group_discussion(discussion_id) DEFERRABLE INITIALLY DEFERRED NOT NULL,
+    discussion_id UUID REFERENCES user_group_discussion(discussion_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL,
     comment_id UUID NOT NULL,
 
     name text NOT NULL,
-    creator_id text REFERENCES user_profile(id) DEFERRABLE INITIALLY DEFERRED NOT NULL
+    creator_id text REFERENCES user_profile(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL
+);
+
+CREATE TABLE user_group_discussion_comment_like (
+    PRIMARY KEY (comment_id, liker_id),
+    comment_id UUID REFERENCES user_group_discussion_comment(comment_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL,
+    liker_id text REFERENCES user_profile(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NOT NULL
 );
