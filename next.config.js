90eeda0fb97220874b8ddf9256c445979cce65f6
Sua Qi Rong <99239071+Soqoro@users.noreply.github.com>
2023-10-27 18:41:27 +0800

    Feature: Added book display on home (#27)
    * Added get request to home
    
    * Feature: Homepage book display

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["covers.openlibrary.org", "m.media-amazon.com"],
+  },
+};
 
 module.exports = nextConfig;
